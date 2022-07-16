38c5bbd9959117ced4ecd7570fa7e1e6050f5b4b
Tamim Mahmud <internationalinf24@gmail.com>
2022-07-16 23:45:20 +0600

    user async to input data in JS

--- async.js
+++ async.js
@@ -1,7 +1,32 @@
-async function fullName (name){
-    return "Hello "+ name;
-}
+// async function fullName (name){
+//     return "Hello "+ name;
+// }
 
-const myName = fullName("Mofiz!");
-console.log(myName);
-myName.then(res => console.log(res));
+// const { retry } = require("async");
+
+// const myName = fullName("Mofiz!");
+// console.log(myName);
+// myName.then(res => console.log(res));
+
+async function allusers(){
+    const response = await fetch('https://jsonplaceholder.typicode.com/users');
+    const data = await response.json();
+    allUsers(data);
+    return data;
+}
+allusers();
+
+function allUsers(data){
+    const singleUser = data.map(data => data.username);
+    const ul = document.getElementById("usersid");
+    
+    for (let i = 0; i < singleUser.length; i++) {
+        const element = singleUser[i];
+
+        const li = document.createElement("li");
+        li.innerText= element;
+
+        ul.appendChild(li);
+        
+    }
+}
